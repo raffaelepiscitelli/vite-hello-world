ef5441c51b1c79be9a6f7fe5bd548fb88f697be9
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-12-13 21:38:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped></style>
